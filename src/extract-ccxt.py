7cd7f6ce69f54e988604d88c6c9ec9d50942f1a3
loukkanos <loukkanoss@gmail.com>
2025-04-12 16:12:34 -0400

    feat: configuração do schema.yml e coluna data_extracao no extract-ccxt

--- src/extract-ccxt.py
+++ src/extract-ccxt.py
@@ -3,10 +3,11 @@ import pandas as pd
 import os
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
+from datetime import datetime
 
+# carregar variáveis de ambiente
 load_dotenv()
-
-BD_POSTGRES=os.environ.get("DIRECT_URL")
+BD_POSTGRES = os.environ.get("DIRECT_URL")
 engine = create_engine(BD_POSTGRES)
 
 # lista das criptos para serem analisadas
@@ -18,10 +19,10 @@ criptos = [
     'AXS/USDT', 'SAND/USDT', 'XTZ/USDT', 'AAVE/USDT' 
 ]
 
-# extração dos dados das criptomoedas
+# extract dos dados das criptomoedas
 def extrair_dados(criptos=criptos):
     exchange = ccxt.binance()
-
+    data_extracao = datetime.utcnow()
     dados = []
 
     for cripto in criptos:
@@ -29,24 +30,26 @@ def extrair_dados(criptos=criptos):
         order_book = exchange.fetch_order_book(cripto)
 
         dados.append({
-        'cripto': cripto,
-        'price': ticker['last'],
-        'high': ticker['high'],
-        'low': ticker['low'],
-        'bid': ticker['bid'],  
-        'ask': ticker['ask'],
-        'volume': ticker['baseVolume'],
-        'quote_volume': ticker['quoteVolume'],
-        'order_book_bid': order_book['bids'][0][0] if order_book['bids'] else None,
-        'order_book_ask': order_book['asks'][0][0] if order_book['asks'] else None,
+            'cripto': cripto,
+            'price': ticker['last'],
+            'high': ticker['high'],
+            'low': ticker['low'],
+            'bid': ticker['bid'],  
+            'ask': ticker['ask'],
+            'volume': ticker['baseVolume'],
+            'quote_volume': ticker['quoteVolume'],
+            'order_book_bid': order_book['bids'][0][0] if order_book['bids'] else None,
+            'order_book_ask': order_book['asks'][0][0] if order_book['asks'] else None,
+            'data_extracao': data_extracao
         })
 
     return dados
 
-# load dos dados extraidos para o postgres
+# load dos dados extraídos para o Postgres
 def carregar_dados(dados, schema='dw'):
     df = pd.DataFrame(dados)
-    df.to_sql("bronze-criptosinfo", engine, schema='dw', if_exists="replace", index=False)
+    df.to_sql("bronze-criptosinfo", engine, schema=schema, if_exists="append", index=False)
+
 
 if __name__ == "__main__":
     criptodados = extrair_dados()
